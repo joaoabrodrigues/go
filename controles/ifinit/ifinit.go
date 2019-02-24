654504731f360cd26ca92aeee39d55530f3f54e7
joaoabrodrigues <joaoabrodrigues@outlook.com>
2019-02-24 16:07:39 -0300

    if init

---
+++ controles/ifinit/ifinit.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(10)
+}
+
+func main() {
+	if i := numeroAleatorio(); i > 5 { // também suportado no switch
+		fmt.Println("Ganhou!!!", i)
+	} else {
+		fmt.Println("Perdeu!!!", i)
+	}
+
+	// fmt.Println("Perdeu!!!", i) -> não acessível
+}
