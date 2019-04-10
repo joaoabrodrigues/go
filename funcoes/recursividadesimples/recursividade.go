740e463782bad7272bce9f62cc9f82d4bd985288
joaoabrodrigues <joaoabrodrigues@outlook.com>
2019-04-09 22:55:43 -0300

    recursion

---
+++ funcoes/recursividadesimples/recursividade.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func fatorial(n uint) uint {
+	switch {
+	case n == 0:
+		return 0
+	default:
+		return n * fatorial(n-1)
+	}
+}
+
+func main() {
+	result := fatorial(5)
+	fmt.Println(result)
+}
