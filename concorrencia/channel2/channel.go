8b2f82f12eca21f87fb543201b91a54787b46346
joaoabrodrigues <joaoabrodrigues@outlook.com>
2019-04-20 18:35:30 -0300

    channel and goroutines

---
+++ concorrencia/channel2/channel.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Channel (canal) - é a forma de comunicação entre goroutines
+// channel é um tipo da linguagem
+
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+	c <- 3 * base
+
+	time.Sleep(time.Second)
+	c <- 4 * base
+}
+
+func main() {
+	ch := make(chan int)
+	go doisTresQuatroVezes(2, ch)
+	fmt.Println("A")
+
+	a, b := <-ch, <-ch // recebendo dados do canal
+	fmt.Println("B")
+	fmt.Println(a, b)
+
+	fmt.Println(<-ch)
+}
